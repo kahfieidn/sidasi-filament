57e491f23f682bc05500a8b4cf4903a5e67d977c
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-01-13 15:44:53 +0700

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -482,18 +482,18 @@
 
 
     <!-- Scripts -->
-    <script src="{{ ('/assets/js/vendor/modernizr-3.11.2.min.js') }}"></script>
-    <script src="{{ ('/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ ('/assets/js/vendor/jquery-migrate-3.3.2.min.js') }}"></script>
-    <script src="{{ ('/assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ ('/assets/js/plugins/swiper-bundle.min.js') }}"></script>
-    <script src="{{ ('/assets/js/ajax-contact.js') }}"></script>
-    <script src="{{ ('/assets/js/plugins/ajax-mailchimp.js') }}"></script>
-    <script src="{{ ('/assets/js/plugins/aos.js') }}"></script>
-    <script src="{{ ('/assets/js/plugins/scroll-up.js') }}"></script>
-    <script src="{{ ('/assets/js/plugins/waypoints.js') }}"></script>
-    <script src="{{ ('/assets/js/plugins/jquery.selectric.min.js') }}"></script>
-    <script src="{{ ('/assets/js/main.js') }}"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/ajax-contact.js"></script>
+    <script src="assets/js/plugins/ajax-mailchimp.js"></script>
+    <script src="assets/js/plugins/aos.js"></script>
+    <script src="assets/js/plugins/scroll-up.js"></script>
+    <script src="assets/js/plugins/waypoints.js"></script>
+    <script src="assets/js/plugins/jquery.selectric.min.js"></script>
+    <script src="assets/js/main.js"></script>
 
 </body>
 

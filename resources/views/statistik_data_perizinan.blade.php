9f867d5863b9032e7dc42e262dbf2d76c89b27df
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-01-14 08:15:49 +0700

    fix nav

--- resources/views/statistik_data_perizinan.blade.php
+++ resources/views/statistik_data_perizinan.blade.php
@@ -19,6 +19,7 @@
 
 
 <body>
+
     <!-- Modal -->
     <div class="modal offcanvas-modal fade" id="offcanvas-modal">
         <div class="modal-dialog offcanvas-dialog">
@@ -26,54 +27,18 @@
                 <div class="modal-header offcanvas-header">
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <!-- offcanvas-form-wrap start -->
-                <div class="offcanvas-form-wrap">
-                    <form action="#" class="offcanvas-form position-relative">
-                        <input class="form-control" type="text" placeholder="Enter your search key ... " />
-                        <button class="btn btn-warning">search</button>
-                    </form>
-                </div>
-                <!-- offcanvas-form-wrap end -->
                 <!-- offcanvas-menu start -->
                 <nav id="offcanvas-menu" class="offcanvas-menu">
                     <ul>
                         <li>
-                            <a href="/">Home</a>
+                            <a href="/">Homepage</a>
                             <!-- add your sub menu here -->
                         </li>
                         <li>
-                            <a href="service-details.html">Services</a>
+                            <a href="{{route('statistik_data_perizinan')}}">Statistik Data Perizinan</a>
                         </li>
                         <li>
-                            <a href="case-details.html">Case Studies</a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)">Blog</a>
-                            <ul>
-                                <li>
-                                    <a href="blog-details.html">blog details</a>
-                                </li>
-                                <li>
-                                    <a href="blog-grid-3-column.html">blog grid 3 column</a>
-                                </li>
-                                <li>
-                                    <a href="blog-left-sidebar.html">blog left sidebar</a>
-                                </li>
-                                <li>
-                                    <a href="blog-right-sidebar.html">blog right sidebar</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)">Pages</a>
-                            <ul>
-                                <li><a href="about-us.html">about us</a></li>
-                                <li><a href="faq.html">faq</a></li>
-                                <li><a href="404.html">404 not found!</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="contact-us.html">Contact</a>
+                            <a href="{{route('statistik_data_investasi')}}">Statistik Data Investasi</a>
                         </li>
                     </ul>
 
@@ -95,8 +60,6 @@
                     </div>
                 </nav>
                 <!-- offcanvas-menu end -->
-
-                <p class="text-gradient mt-3">Let us grow your website traffic.</p>
             </div>
         </div>
     </div>
@@ -137,7 +100,7 @@
                         </a>
                     </div>
                     <div class="col-auto">
-                        <a class="btn btn-warning btn-hover-warning d-none d-sm-inline-block d-lg-none" href="blog-details.html">Analyze Your Site <i class="icofont-arrow-right"></i>
+                        <a class="btn btn-warning btn-hover-warning d-none d-sm-inline-block d-lg-none" href="/app">Login Sebagai Pengelola <i class="icofont-arrow-right"></i>
                         </a>
 
                         <button type="button" class="btn btn-warning offcanvas-toggler" data-bs-toggle="modal" data-bs-target="#offcanvas-modal">

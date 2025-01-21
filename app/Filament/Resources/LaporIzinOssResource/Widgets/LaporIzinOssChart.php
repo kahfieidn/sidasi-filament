822dd3f97a4370c28cc93933bd7f14d315071976
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2025-01-21 08:32:17 +0700

    fix grafik dashboard

--- app/Filament/Resources/LaporIzinOssResource/Widgets/LaporIzinOssChart.php
+++ app/Filament/Resources/LaporIzinOssResource/Widgets/LaporIzinOssChart.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\LaporIzinOssResource\Widgets;
 
 use App\Models\LaporIzinOss;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\Auth;
 
 class LaporIzinOssChart extends ChartWidget
 {
@@ -12,18 +13,18 @@ class LaporIzinOssChart extends ChartWidget
     protected function getData(): array
     {
 
-        $januari = LaporIzinOss::whereBulan('Januari')->sum('jumlah_data');
-        $februari = LaporIzinOss::whereBulan('Februari')->sum('jumlah_data');
-        $maret = LaporIzinOss::whereBulan('Maret')->sum('jumlah_data');
-        $april = LaporIzinOss::whereBulan('April')->sum('jumlah_data');
-        $mei = LaporIzinOss::whereBulan('Mei')->sum('jumlah_data');
-        $juni = LaporIzinOss::whereBulan('Juni')->sum('jumlah_data');
-        $juli = LaporIzinOss::whereBulan('Juli')->sum('jumlah_data');
-        $agustus = LaporIzinOss::whereBulan('Agustus')->sum('jumlah_data');
-        $september = LaporIzinOss::whereBulan('September')->sum('jumlah_data');
-        $oktober = LaporIzinOss::whereBulan('Oktober')->sum('jumlah_data');
-        $november = LaporIzinOss::whereBulan('November')->sum('jumlah_data');
-        $desember = LaporIzinOss::whereBulan('Desember')->sum('jumlah_data');
+        $januari = LaporIzinOss::whereBulan('Januari')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $februari = LaporIzinOss::whereBulan('Februari')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $maret = LaporIzinOss::whereBulan('Maret')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $april = LaporIzinOss::whereBulan('April')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $mei = LaporIzinOss::whereBulan('Mei')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $juni = LaporIzinOss::whereBulan('Juni')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $juli = LaporIzinOss::whereBulan('Juli')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $agustus = LaporIzinOss::whereBulan('Agustus')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $september = LaporIzinOss::whereBulan('September')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $oktober = LaporIzinOss::whereBulan('Oktober')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $november = LaporIzinOss::whereBulan('November')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
+        $desember = LaporIzinOss::whereBulan('Desember')->whereTahun(2024)->whereUserId(Auth::id())->sum('jumlah_data');
 
         return [
             'datasets' => [
--- app/Filament/Resources/LaporIzinResource/Widgets/LaporIzinChart.php
+++ app/Filament/Resources/LaporIzinResource/Widgets/LaporIzinChart.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\LaporIzinResource\Widgets;
 
 use App\Models\LaporIzin;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\Auth;
 
 class LaporIzinChart extends ChartWidget
 {
@@ -12,18 +13,18 @@ class LaporIzinChart extends ChartWidget
     protected function getData(): array
     {
 
-        $januari = LaporIzin::whereMonth('tanggal_izin', 1)->get();
-        $februari = LaporIzin::whereMonth('tanggal_izin', 2)->get();
-        $maret = LaporIzin::whereMonth('tanggal_izin', 3)->get();
-        $april = LaporIzin::whereMonth('tanggal_izin', 4)->get();
-        $mei = LaporIzin::whereMonth('tanggal_izin', 5)->get();
-        $juni = LaporIzin::whereMonth('tanggal_izin', 6)->get();
-        $juli = LaporIzin::whereMonth('tanggal_izin', 7)->get();
-        $agustus = LaporIzin::whereMonth('tanggal_izin', 8)->get();
-        $september = LaporIzin::whereMonth('tanggal_izin', 9)->get();
-        $oktober = LaporIzin::whereMonth('tanggal_izin', 10)->get();
-        $november = LaporIzin::whereMonth('tanggal_izin', 11)->get();
-        $desember = LaporIzin::whereMonth('tanggal_izin', 12)->get();
+        $januari = LaporIzin::whereMonth('tanggal_izin', 1)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $februari = LaporIzin::whereMonth('tanggal_izin', 2)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $maret = LaporIzin::whereMonth('tanggal_izin', 3)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $april = LaporIzin::whereMonth('tanggal_izin', 4)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $mei = LaporIzin::whereMonth('tanggal_izin', 5)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $juni = LaporIzin::whereMonth('tanggal_izin', 6)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $juli = LaporIzin::whereMonth('tanggal_izin', 7)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $agustus = LaporIzin::whereMonth('tanggal_izin', 8)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $september = LaporIzin::whereMonth('tanggal_izin', 9)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $oktober = LaporIzin::whereMonth('tanggal_izin', 10)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $november = LaporIzin::whereMonth('tanggal_izin', 11)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
+        $desember = LaporIzin::whereMonth('tanggal_izin', 12)->whereYear('tanggal_izin', 2024)->whereUserId(Auth::id())->get();
 
         return [
 
